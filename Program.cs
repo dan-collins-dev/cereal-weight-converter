c7a49c0c30da53bbac147ac37fd3f7b36456f9d8
Dan Collins <dan.collins.dev@gmail.com>
2024-12-20 22:17:25 -0500

    finished. made sure to account for non numeric input

--- Program.cs
+++ Program.cs
@@ -16,7 +16,33 @@ namespace CerealWeightConverter
     {
         static void Main(string[] args) 
         {
+            float metricTon = 35273.92f;
 
+            Console.Write("Enter the weight of a box of cereal in ounces: ");
+            string input = Console.ReadLine();
+            if (float.TryParse(input, out _)) 
+            {
+                float boxWeightInTons = OuncesToMetricTons(float.Parse(input), metricTon);
+                float boxesNeededForTon = MetricTonsToOunces(float.Parse(input), metricTon);
+                Console.WriteLine("");
+                Console.WriteLine($"This cereal's weight in metric tons is {boxWeightInTons}.");
+                Console.WriteLine($"You need roughly {boxesNeededForTon} boxes of this cereal to equal a metric ton.");
+            }
+            else 
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Input was not a valid number.");
+            }
+        }
+
+        static float OuncesToMetricTons(float weight, float metricTon)
+        {
+            return weight / metricTon;
+        }
+
+        static float MetricTonsToOunces(float weight, float metricTon)
+        {
+            return metricTon / weight;
         }
     } 
 }
